6928eac9982e6dbca4a497e701767c84d91b39bf
Gaultheria <valeriaburina@gmail.com>
2023-01-10 19:07:44 +0100

    gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+       <a class="gallery__link" href="${original}" onclick="return false">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+         /> 
+        </a> 
+      </div>`,
+  ''
+);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', onClick);
+let instance;
+function onClick(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  evt.preventDefault();
+
+  const source = evt.target.dataset.source;
+  instance = basicLightbox.create(`
+    <div class="modal">
+      <img src='${source}' />
+    </div>
+    `);
+  instance.show();
+
+  return instance;
+}
+
+gallery.addEventListener('keydown', onEsc);
+function onEsc(event) {
+  if (instance.visible()) {
+    if (event.keyCode === 27) {
+      instance.close();
+    }
+  }
+}
